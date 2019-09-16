ce5495aa5e659e9ac5f2419f52e88abaa2118368
IgorValetskii <blrapok@gmail.com>
2019-09-16 14:00:54 +0300

    fix request method

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const req = new XMLHttpRequest();
-
 const params = {
     access_key: 'b789446123c59c2315cca1e291e75c57',
     query: 'Minsk'
 };
 
-req.open('GET',`http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`,true );
-req.send();
-req.responseType ='text';
-req.onload = function() {
-    const apiResponse = req.response;
-    const result = JSON.parse(apiResponse);
+const func = async load =>{
+    let promise = await fetch(`http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`, {method:`GET`});
+    let result =  await promise.json();
     console.log(`Current temparture in ${result.location.name} is ${result.current.temperature}℃`);
 };
+func();
+
+
+// const req = new XMLHttpRequest();
+
+// req.open('GET',`http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`,true );
+// req.send();
+// req.responseType ='text';
+// req.onload = function() {
+//     const apiResponse = req.response;
+//     const result = JSON.parse(apiResponse);
+//     console.log(`Current temparture in ${result.location.name} is ${result.current.temperature}℃`);
+// };
+
